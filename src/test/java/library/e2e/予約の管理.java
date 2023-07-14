91ce88cb9510064f40b8a15d57459e91768bf182
setoguchi <setoguchi.takuma@gmail.com>
2023-07-14 19:12:17 +0900

    予約の管理テストをselenideでの記述に変更

--- src/test/java/library/e2e/予約の管理.java
+++ src/test/java/library/e2e/予約の管理.java
@@ -1,5 +1,6 @@
 package library.e2e;
 
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -12,167 +13,141 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import java.time.Duration;
 import java.util.List;
 
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class 予約の管理 {
-    WebDriver driver;
 
     @LocalServerPort
     int port;
-    @BeforeEach
-    public void setup() {
-        driver = new ChromeDriver();
-    }
-
-    @AfterEach
-    public void teardown() {
-        if (driver != null) {
-            driver.quit();
-        }
-    }
 
     @Test
     @Timeout(10)
     public void 図書の予約_取置_貸出_返却() {
-        // メニュー画面
-//        driver.get("http://localhost:%d".formatted(port));
-        driver.get("http://localhost:8080");
+        open("http://localhost:%d".formatted(port));
         {
             // 予約画面 (図書の検索)
-            WebElement 本の予約リンク = driver.findElement(By.id("link-reservation-for-member"));
+            var 本の予約リンク = $(By.id("link-reservation-for-member"));
             本の予約リンク.click();
         }
 
         // 予約画面 (図書の検索)
         {
-            new WebDriverWait(driver, Duration.ofSeconds(3))
-                    .until(driver -> driver.findElement(By.id("header-search-result")));
+            $(By.id("header-search-result")).should(Condition.appear);
 
-            WebElement キーワード入力欄 = driver.findElement(By.id("value"));
-            キーワード入力欄.sendKeys("現場");
+            var キーワード入力欄 = $(By.id("value"));
+            キーワード入力欄.setValue("現場");
 
-            WebElement 検索ボタン = driver.findElement(By.id("button-search"));
+            var 検索ボタン = $(By.id("button-search"));
             検索ボタン.click();
         }
 
         // 予約画面 (図書の検索結果)
         {
-            WebElement 検索結果表 = new WebDriverWait(driver, Duration.ofSeconds(3))
-                    .until(driver -> driver.findElement(By.id("table-search-result")));
+            var 検索結果表 = $(By.id("table-search-result")).should(Condition.appear);
 
-            List<WebElement> 検索結果一覧 = 検索結果表.findElement(By.tagName("tbody")).findElements(By.tagName("tr"));
+            var 検索結果一覧 = 検索結果表.find(By.tagName("tbody")).findAll(By.tagName("tr"));
             assertThat(検索結果一覧.size(), is(1));
-            WebElement 予約リンク = 検索結果一覧.get(0).findElements(By.tagName("td")).get(0).findElement(By.tagName("a"));
+
+            var 予約リンク = 検索結果一覧.first().findAll(By.tagName("td")).first().find(By.tagName("a"));
 
             予約リンク.click();
         }
 
         // 予約画面
         {
-            WebElement 予約ボタン = new WebDriverWait(driver, Duration.ofSeconds(3))
-                    .until(driver -> driver.findElement(By.id("button-reserve")));
+            var 予約ボタン = $(By.id("button-reserve")).should(Condition.appear);
 
-            WebElement 会員番号入力欄 = driver.findElement(By.id("value"));
-            会員番号入力欄.sendKeys("1");
+            var 会員番号入力欄 = $(By.id("value"));
+            会員番号入力欄.setValue("1");
             予約ボタン.click();
         }
 
         // 予約完了
         {
-            WebElement 完了メッセージ = new WebDriverWait(driver, Duration.ofSeconds(3))
-                    .until(driver -> driver.findElement(By.className("notification")));
+            var 完了メッセージ = $(By.className("notification")).should(Condition.appear);
 
             assertThat(完了メッセージ.getText(), is("本を予約しました。"));
 
-            WebElement ホームリンク = driver.findElement(By.id("link-to-home"));
+            var ホームリンク = $(By.id("link-to-home"));
             ホームリンク.click();
         }
 
         // ホーム画面
         {
-            WebElement 予約の管理リンク = new WebDriverWait(driver, Duration.ofSeconds(3))
-                    .until(driver -> driver.findElement(By.id("link-reservations")));
-
+            var 予約の管理リンク = $(By.id("link-reservations")).should(Condition.appear);
             予約の管理リンク.click();
         }
 
         // 予約の管理画面
         {
-            var 予約表 = new WebDriverWait(driver, Duration.ofSeconds(3))
-                    .until(driver -> driver.findElement(By.id("table-reservations")));
-
-            List<WebElement> 予約の一覧 = 予約表.findElement(new By.ByTagName("tbody")).findElements(By.tagName("tr"));
+            var 予約表 = $(By.id("table-reservations")).should(Condition.appear);
+            var 予約の一覧 = 予約表.find(By.tagName("tbody")).findAll(By.tagName("tr"));
 
             assertThat(予約の一覧.size(), is(1));
 
-            WebElement 取置ボタン = 予約の一覧.get(0).findElements(By.tagName("button")).get(0);
+            var 取置ボタン = 予約の一覧.get(0).findAll(By.tagName("button")).first();
 
             取置ボタン.click();
         }
 
         // 取置き画面
         {
-            WebElement 取置き実行ボタン = new WebDriverWait(driver, Duration.ofSeconds(3))
-                    .until(driver -> driver.findElement(By.id("button-retention")));
+            var 取置き実行ボタン = $(By.id("button-retention")).should(Condition.appear);
 
-            WebElement 所蔵品番号入力欄 = driver.findElement(By.id("item"));
-            所蔵品番号入力欄.sendKeys("1-A");
+            var 所蔵品番号入力欄 = $(By.id("item"));
+            所蔵品番号入力欄.setValue("1-A");
 
             取置き実行ボタン.click();
             // ホーム画面
-            WebElement ホームリンク = driver.findElement(By.id("link-to-home"));
+            var ホームリンク = $(By.id("link-to-home"));
             ホームリンク.click();
         }
 
-
         // メニュー画面
         {
-            WebElement 取置きの管理リンク = new WebDriverWait(driver, Duration.ofSeconds(3))
-                    .until(driver -> driver.findElement(By.id("link-retentions")));
+            var 取置きの管理リンク = $(By.id("link-retentions"));
 
             取置きの管理リンク.click();
         }
 
         // 取置き画面
         {
-            WebElement 取置き表 = new WebDriverWait(driver, Duration.ofSeconds(3))
-                    .until(driver -> driver.findElement(By.id("table-retentions")));
-            List<WebElement> 取置一覧 = 取置き表.findElement(By.tagName("tbody")).findElements(By.tagName("tr"));
+            var 取置き表 = $(By.id("table-retentions")).should(Condition.appear);
+            var 取置一覧 = 取置き表.find(By.tagName("tbody")).findAll(By.tagName("tr"));
             assertThat(取置一覧.size(), is(1));
 
-            WebElement 貸出ボタン = 取置一覧.get(0).findElement(By.tagName("button"));
+            var 貸出ボタン = 取置一覧.first().find(By.tagName("button"));
             貸出ボタン.click();
 
             // ホーム画面
-            WebElement ホームリンク = driver.findElement(By.id("link-to-home"));
+            var ホームリンク = $(By.id("link-to-home"));
             ホームリンク.click();
         }
 
         // メニュー画面
         {
-            WebElement 貸出と返却リンク = new WebDriverWait(driver, Duration.ofSeconds(3))
-                    .until(driver -> driver.findElement(By.id("link-loan-return")));
+            var 貸出と返却リンク = $(By.id("link-loan-return")).should(Condition.appear);
             貸出と返却リンク.click();
         }
 
         // 貸出と返却画面
         {
-            WebElement 返却リンク = new WebDriverWait(driver, Duration.ofSeconds(3))
-                    .until(driver -> driver.findElement(By.id("link-return")));
+            var 返却リンク = $(By.id("link-return")).should(Condition.appear);
             返却リンク.click();
         }
 
         // 貸出図書の返却
         {
-            WebElement 返却登録ボタン = new WebDriverWait(driver, Duration.ofSeconds(3))
-                    .until(driver -> driver.findElement(By.id("button-return")));
-            WebElement 返却所蔵品番号入力欄 = driver.findElement(By.id("itemNumber.value"));
+            var 返却登録ボタン = $(By.id("button-return")).should(Condition.appear);
+            var 返却所蔵品番号入力欄 = $(By.id("itemNumber.value"));
 
-            返却所蔵品番号入力欄.sendKeys("1-A");
+            返却所蔵品番号入力欄.setValue("1-A");
             返却登録ボタン.click();
         }
-
     }
+
 }
